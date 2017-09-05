dbef5f8d83ee2e23d67f68634f3f0fa3e008ad6d
Alexander Gubanov <greatehop@gmail.com>
2017-09-05 16:43:31 +0300

    Update bash start script

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
-PATH_PROJECT=""
+PATH_FULL=$(realpath $0)
+PATH_PROJECT=$(dirname ${PATH_FULL})
 
-virtualenv venv
-source venv/bin/activate
-pip install -r requirements.txt
+if [ ! -d ${PATH_PROJECT}/venv ]; then
+    cd ${PATH_PROJECT}
+    virtualenv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+    deactivate
+fi
+
+${PATH_PROJECT}/venv/bin/python ${PATH_PROJECT}/run.py > /dev/null 2>&1 &
+
 
